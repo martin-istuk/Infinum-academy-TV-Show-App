06b1e326210087f94adaf60fcf631d3e35e8e55f
martin-istuk <45366040+martin-istuk@users.noreply.github.com>
2022-10-19 15:44:41 +0200

    removed custom name validator

--- src/app/validators/email-validator.directive.ts
+++
@@ -1,8 +0,0 @@
-import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
-
-export function emailMartinValidator(nameRe: RegExp): ValidatorFn {
-	return (control: AbstractControl): ValidationErrors | null => {
-		const forbidden = nameRe.test(control.value);
-		return forbidden ? { forbiddenName: { value: control.value } } : null;
-	};
-}
