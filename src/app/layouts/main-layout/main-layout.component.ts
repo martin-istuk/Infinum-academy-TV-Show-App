7f399901c93dfe429b18f4258a552ede461554c4
martin-istuk <45366040+martin-istuk@users.noreply.github.com>
2022-11-07 19:11:07 +0100

    fixed mainLayout & navigation

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,8 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, ChangeDetectorRef } from "@angular/core";
+import { MediaMatcher } from "@angular/cdk/layout";
 
 @Component({
 	selector: "app-main-layout",
 	templateUrl: "./main-layout.component.html",
 	styleUrls: ["./main-layout.component.scss"]
 })
-export class MainLayoutComponent {}
+export class MainLayoutComponent implements OnDestroy {
+	public mobileQuery: MediaQueryList;
+	private _mobileQueryListener: () => void;
+
+	constructor(media: MediaMatcher, changeDetectorRef: ChangeDetectorRef) {
+		this.mobileQuery = media.matchMedia("(max-width: 1060px)");
+		this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+		this.mobileQuery.addListener(this._mobileQueryListener);
+	}
+
+	ngOnDestroy(): void {
+		this.mobileQuery.removeListener(this._mobileQueryListener);
+	}
+}
